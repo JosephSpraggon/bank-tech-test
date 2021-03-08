89f30636136926a1fb9c5b138db6756418ebb0ef
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-08 12:09:07 +0000

    an amount can now be withdrawn from an account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,8 @@ require 'account'
 
 describe Account do
 
+  let(:test_account) {Account.new(500)}
+
   it 'can create an account object' do
     expect(subject).to be_an_instance_of(Account)
   end
@@ -15,6 +17,15 @@ describe Account do
     it 'can add an amount to the balance' do
       subject.deposit(500)
       expect(subject.balance).to eq(500)
+    end
+
+  end
+
+  context '#withdraw' do 
+
+    it 'can minus an amount from the balance' do
+      test_account.withdraw(100)
+      expect(test_account.balance).to eq(400)
     end
 
   end

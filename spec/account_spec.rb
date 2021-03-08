7d7dc17972574f44bfff406b6321e0bbdd8d0236
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-08 11:46:38 +0000

    an account object has a default balance of zero

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,8 @@ describe Account do
     expect(subject).to be_an_instance_of(Account)
   end
 
+  it 'a new account object has default balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
 end
